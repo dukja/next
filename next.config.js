af2fb354d0a0edbf4d4d9eb30cd96a35d8f3090c
dukja <100657522+dukja@users.noreply.github.com>
2022-05-06 08:46:37 +0900

    test

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  assetPrefix: !debug ? '/project/' : '',
+  assetPrefix: !debug ? '/next/' : '',
   images: {
     loader: 'akamai',
     path: '/',
