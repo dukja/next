3793570f8275e9cdebda3612d8e79c1ab573ed60
dukja <100657522+dukja@users.noreply.github.com>
2022-05-13 15:51:27 +0900

    organize

--- next.config.js
+++ next.config.js
@@ -10,11 +10,4 @@ module.exports = {
     loader: 'akamai',
     path: '/',
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
-  },
 }
